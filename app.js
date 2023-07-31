96642b02ad1a9dd1e6a6d33e6266a3b473c7aad1
Manimegalai <manimegalai30102@gmail.com>
2023-07-31 22:42:00 +0530

    code added

--- app.js
+++ app.js
@@ -1,40 +1,61 @@
-let container=document.querySelector('#display')
-let title=document.querySelector('#title');
-let content=document.querySelector('#content');
-let nodeList=document.createElement('li')
-let submitBtn=document.querySelector('#submit')
+let container = document.querySelector("#display");
+let title = document.querySelector("#title");
+let content = document.querySelector("#content");
+let nodeList = document.createElement("li");
+let submitBtn = document.querySelector("#submit");
+let form = document.getElementById("notesForm");
+let array = [];
 
-submitBtn.addEventListener('click',()=>{
-        const value1= title.value;
-        const value2= content.value;
-        
-        nodeList.textContent= value1 + " " + value2 ;
-        myFunction()
-        myFunction1()
-    
-    })
-    function myFunction() {
-        var x = document.createElement("BUTTON");
-        var t = document.createTextNode("Delete");
-        x.appendChild(t);
-        nodeList.appendChild(x)
-      }
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const value1 = title.value;
 
-      function myFunction1() {
-        var x = document.createElement("BUTTON");
-        var t = document.createTextNode("edit");
-        x.appendChild(t);
-        nodeList.appendChild(x);
-      
-      }
-        
-      function onDelete(li) {
-            if (confirm("Are you sure to delete this note?")) {
-              const parentUl = document.getElementById("display");
-              parentUl.removeChild(li);
-            }
-          }
-    
+  let heading = document.createElement("h4");
+  heading.textContent = value1;
 
-    container.appendChild(nodeList)
-    
+  nodeList.appendChild(heading);
+
+  const value2 = content.value;
+  let para = document.createElement("p");
+
+  para.textContent = value2;
+
+  nodeList.appendChild(para);
+
+  const obj = { title: value1, des: value2 };
+  console.log(obj);
+
+  array.push(obj);
+  // console.log(array);
+
+  var del = document.createElement("BUTTON");
+  del.innerHTML = "delete";
+  nodeList.appendChild(del);
+
+  del.addEventListener("click", () => {
+    nodeList.removeChild(heading);
+    nodeList.removeChild(para);
+    nodeList.removeChild(del);
+    nodeList.removeChild(edit);
+  });
+
+  var edit = document.createElement("BUTTON");
+  edit.innerHTML = "edit";
+  nodeList.appendChild(edit);
+  edit.addEventListener("click", () => {
+    title.value = obj.title;
+    content.value = obj.des;
+
+    // submitBtn.innerHTML="update";
+
+    nodeList.removeChild(heading);
+    nodeList.removeChild(para);
+    nodeList.removeChild(del);
+    nodeList.removeChild(edit);
+  });
+
+  title.value = ""; //reset title
+  content.value = ""; //reset content
+});
+
+container.appendChild(nodeList);

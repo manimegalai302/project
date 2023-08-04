6f541cb012bbd9f8927fe1b62cb7879ac860c9c8
Manimegalai <manimegalai30102@gmail.com>
2023-08-04 09:30:23 +0530

    added

--- app.js
+++ app.js
@@ -1,67 +1,3 @@
-// let container = document.querySelector("#display");
-// let title = document.querySelector("#title");
-// let content = document.querySelector("#content");
-// let unOrderList=document.createElement("ul");
-// let nodeList = document.createElement("li");
-// let submitBtn = document.querySelector("#submit");
-// let form = document.getElementById("notesForm");
-// let array = [];
-
-// form.addEventListener("submit", (event) => {
-//   event.preventDefault();
-//   const value1 = title.value;
-
-//   let heading = document.createElement("h4");
-//   heading.textContent = value1;
-
-//   nodeList.appendChild(heading);
-
-//   const value2 = content.value;
-//     let para = document.createElement("p");
-//     para.textContent = value2;
-//     nodeList.appendChild(para);
-
-
-//   const obj = { title: value1, des: value2 };
-//   console.log(obj);
-
-//   array.push(obj);
-//   // console.log(array);
-
-//   var del = document.createElement("BUTTON");
-//   del.innerHTML = "delete";
-//   nodeList.appendChild(del);
-
-//   del.addEventListener("click", () => {
-//     nodeList.removeChild(heading);
-//     nodeList.removeChild(para);
-//     nodeList.removeChild(del);
-//     nodeList.removeChild(edit);
-//   });
-
-//   var edit = document.createElement("BUTTON");
-//   edit.innerHTML = "edit";
-//   nodeList.appendChild(edit);
-//   edit.addEventListener("click", () => {
-//     title.value = obj.title;
-//     content.value = obj.des;
-
-//     // submitBtn.innerHTML="update";
-
-//     nodeList.removeChild(heading);
-//     nodeList.removeChild(para);
-//     nodeList.removeChild(del);
-//     nodeList.removeChild(edit);
-//   });
-
-//   title.value = ""; //reset title
-//   content.value = ""; //reset content
-// });
-
-// container.appendChild(nodeList);
-
-     
-
 let container = document.querySelector("#display");
 let title = document.querySelector("#title");
 let content = document.querySelector("#content");
@@ -69,11 +5,16 @@ let unOrderList = document.createElement("ul");
 let nodeList = document.createElement("li");
 let submitBtn = document.querySelector("#submit");
 let form = document.getElementById("notesForm");
-let array = JSON.parse(localStorage.getItem('notes')) || []; // Check for existing notes in local storage
+let array=[];
 
+window.onload =() => {
+array = JSON.parse(localStorage.getItem('notes')) || []; // Check for existing notes in local storage
+}
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const value1 = title.value;
+
+  
 
   let heading = document.createElement("h4");
   heading.textContent = value1;
